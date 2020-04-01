1f7e0710f7e2415c0b938da2947df8afc2af7c3e
Aram Koukia <aramkoukia@gmail.com>
2020-04-01 09:34:32 -0700

    using template

--- src/components/Header/HeaderLinks.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import withStyles from '@material-ui/core/styles/withStyles';
-import headerLinksStyle from '../../assets/jss/material-dashboard-react/components/headerLinksStyle';
-
-function HeaderLinks() {
-  return (<div />);
-}
-
-export default withStyles(headerLinksStyle)(HeaderLinks);
--- src/index.js
+++ src/index.js
@@ -1,34 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createBrowserHistory } from 'history';
-import {
-  Router, Route, Switch,
-} from 'react-router-dom';
-
-import './assets/css/material-dashboard-react.css';
-import indexRoutes from './routes/index';
-import Auth from './services/Auth';
-
-const hist = createBrowserHistory();
-
-function permissionsChanged() {
-  // console.log('permissionsChanged');
-}
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
-  // eslint-disable-next-line react/jsx-filename-extension
-  <Router history={hist}>
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        if (Auth.isSignedIn()) {
-          return (
-            <Route path={prop.path} component={prop.component} key={key}  permissionsChanged={permissionsChanged} />
-          );
-        }
-        return <Route path={prop.path} component={prop.component} key={key} permissionsChanged={permissionsChanged} />;
-        // return <Redirect from={prop.path} to="/login" key={key} />;
-      })}
-    </Switch>
-  </Router>,
-  document.getElementById('root'),
+  <App />,
+  document.getElementById("root")
 );
