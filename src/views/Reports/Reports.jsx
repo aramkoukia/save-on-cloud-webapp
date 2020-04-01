6b9f77d9e23742da61d2869470d095ee60c74f3b
Aram Koukia <aramkoukia@gmail.com>
2020-03-31 18:48:52 -0700

    convert to function

--- src/views/Reports/Reports.jsx
+++ src/views/Reports/Reports.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import ChartistGraph from 'react-chartist';
-import withStyles from '@material-ui/core/styles/withStyles';
 import { dailySalesChart } from '../../variables/charts';
-import dashboardStyle from '../../assets/jss/material-dashboard-react/views/dashboardStyle';
 import GridItem from '../../components/Grid/GridItem';
 import GridContainer from '../../components/Grid/GridContainer';
 import Card from '../../components/Card/Card';
@@ -11,69 +8,48 @@ import CardHeader from '../../components/Card/CardHeader';
 import CardBody from '../../components/Card/CardBody';
 import ReportService from '../../services/ReportService';
 
-class Reports extends React.Component {
-  constructor(props) {
-    super(props);
+export default function Reports() {
+  const [azureCost, setAzureCost] = React.useState({});
 
-    this.state = { azureCost: {} };
-  }
-
-  componentDidMount() {
-    // this.setState({ loading: true });
-
-    ReportService.getAzureCost()
-      .then((result) => {
-        const azureCost = {
-          labels: result.map(item => item.label),
-          series: [result.map(item => item.value)],
-        };
-        this.setState({ azureCost });
+  ReportService.getAzureCost()
+    .then((result) => {
+      setAzureCost({
+        labels: result.map(item => item.label),
+        series: [result.map(item => item.value)],
       });
-  }
+    });
 
-  render() {
-    const { classes } = this.props;
-    const {
-      azureCost,
-    } = this.state;
-    const options = {
-      seriesBarDistance: 100,
-      reverseData: true,
-      horizontalBars: true,
-      axisY: {
-        offset: 150,
-        scaleMinSpace: 50,
-      },
-      height:500,
-    };
-
-    return (
-      <div>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={12}>
-            <Card chart>
-              <CardHeader color="success">
-                <ChartistGraph
-                  className="ct-chart"
-                  data={azureCost}
-                  type="Bar"
-                  options={options}
-                  listener={dailySalesChart.animation}
-                />
-              </CardHeader>
-              <CardBody>
-                <h4 className={classes.cardTitle}>Resource Cost</h4>
-              </CardBody>
-            </Card>
-          </GridItem>
-        </GridContainer>
-      </div>
-    );
-  }
+  const options = {
+    seriesBarDistance: 100,
+    reverseData: true,
+    horizontalBars: true,
+    axisY: {
+      offset: 150,
+      scaleMinSpace: 50,
+    },
+    height: 500,
+  };
+  // const { ...rest } = props;
+  return (
+    <div>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={12}>
+          <Card chart>
+            <CardHeader color="success">
+              <ChartistGraph
+                className="ct-chart"
+                data={azureCost}
+                type="Bar"
+                options={options}
+                listener={dailySalesChart.animation}
+              />
+            </CardHeader>
+            <CardBody>
+              <h4>Resource Cost</h4>
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    </div>
+  );
 }
-
-Reports.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(dashboardStyle)(Reports);
