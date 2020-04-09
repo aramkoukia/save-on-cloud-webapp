5168494c40b134a0a0048232eb1bc50e663e17b6
Aram Koukia <aramkoukia@gmail.com>
2020-04-09 14:56:00 -0700

    adding account picker

--- src/logged_in/components/reports/ReportFilter.js
+++ src/logged_in/components/reports/ReportFilter.js
@@ -1,14 +1,62 @@
 import React from 'react';
-import { TextField, withTheme } from '@material-ui/core';
+import { withTheme } from '@material-ui/core';
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
-// import DateTimePicker from '../../../shared/components/DateTimePicker';
+import Input from '@material-ui/core/Input';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import ListItemText from '@material-ui/core/ListItemText';
+import Select from '@material-ui/core/Select';
+import Checkbox from '@material-ui/core/Checkbox';
 
 function ReportFilter() {
   const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
+  };
+
+  const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: '100%',
+    },
+    chips: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    chip: {
+      margin: 2,
+    },
+    noLabel: {
+      marginTop: theme.spacing(3),
+    },
+  }));
+
+  const classes = useStyles();
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      },
+    },
+  };
+
+  const names = [
+    'All Accounts',
+    'Enterprise',
+    'Dev/Test',
+    'Production',
+  ];
+
+  const [personName, setPersonName] = React.useState([]);
+
+  const handleChange = (event) => {
+    setPersonName(event.target.value);
   };
 
   return (
@@ -42,6 +90,27 @@ function ReportFilter() {
             }}
           />
         </MuiPickersUtilsProvider>
+
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-mutiple-checkbox-label">Subscriptions/Accounts</InputLabel>
+          <Select
+            labelId="demo-mutiple-checkbox-label"
+            id="demo-mutiple-checkbox"
+            multiple
+            value={personName}
+            onChange={handleChange}
+            input={<Input />}
+            renderValue={(selected) => selected.join(', ')}
+            MenuProps={MenuProps}
+          >
+            {names.map((name) => (
+              <MenuItem key={name} value={name}>
+                <Checkbox checked={personName.indexOf(name) > -1} />
+                <ListItemText primary={name} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
       </form>
     </>
   );
